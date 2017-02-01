19f05886138cbe8fab79208c242e3174762de35e
Aleksei Zaitsev <zaitsev.av.work@gmail.com>
2017-02-01 18:07:25 +0300

    Add hack to calculate signature with another payonline merchant_id and psk

--- lib/payonline/signature.rb
+++ lib/payonline/signature.rb
@@ -29,7 +29,8 @@ module Payonline
     # Prepend params hash with merchant_id
     def prepare_params(params, add_merchant_id = true)
       if add_merchant_id
-        params.reverse_merge!(merchant_id: Payonline.configuration.merchant_id)
+        merchant_id = params[:merchant_id] || Payonline.configuration.merchant_id
+        params.reverse_merge!(merchant_id: merchant_id)
         @keys.unshift(:merchant_id)
       end
 
@@ -39,7 +40,7 @@ module Payonline
     # Prepare params for digest
     def digest_data
       digest_params = @params.slice(*@keys) if @keys.present?
-      digest_params[:private_security_key] = Payonline.configuration.private_security_key
+      digest_params[:private_security_key] = @params[:private_security_key] || Payonline.configuration.private_security_key
 
       digest_params = digest_params.transform_keys { |key| key.to_s.camelize }
       digest_params = fix_transaction_id(digest_params) if @use_hack
